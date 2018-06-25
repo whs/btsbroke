479120135dd240cbbb6835811b37f277953b4c78
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2018-06-26 00:11:25 +0700

    Use BigInteger as primary key

--- tweetdb/models.py
+++ tweetdb/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Tweet(models.Model):
+    id = models.BigIntegerField(primary_key=True)
     user = models.CharField(max_length=15, db_index=True)
     created_time = models.DateTimeField(db_index=True)
     message = models.TextField()
