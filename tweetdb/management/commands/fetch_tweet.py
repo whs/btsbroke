9d9e5fcfd6c3b2d8395c95360651b79d52fc2c49
Manatsawin Hanmongkolchai <manatsawin@wongnai.com>
2018-06-26 09:20:36 +0700

    fetch_tweet: Ignore duplicated entry errors

--- tweetdb/management/commands/fetch_tweet.py
+++ tweetdb/management/commands/fetch_tweet.py
@@ -4,6 +4,7 @@ import datetime
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.core.cache import cache
+from django.db import transaction, IntegrityError
 import twitter
 
 from tweetdb.models import Tweet
@@ -16,7 +17,9 @@ class Command(BaseCommand):
         assert settings.TWITTER_CONSUMER_KEY, "TWITTER_CONSUMER_KEY must be set"
         assert settings.TWITTER_CONSUMER_SECRET, "TWITTER_CONSUMER_SECRET must be set"
         assert settings.TWITTER_ACCESS_TOKEN, "TWITTER_ACCESS_TOKEN must be set"
-        assert settings.TWITTER_ACCESS_TOKEN_SECRET, "TWITTER_ACCESS_TOKEN_SECRET must be set"
+        assert (
+            settings.TWITTER_ACCESS_TOKEN_SECRET
+        ), "TWITTER_ACCESS_TOKEN_SECRET must be set"
 
         client = twitter.Api(
             consumer_key=settings.TWITTER_CONSUMER_KEY,
@@ -33,45 +36,55 @@ class Command(BaseCommand):
         tweets = []
         found_id = set()
 
-        while True:
-            load_count = 200
-            processed = 0
+        with transaction.atomic():
+            while True:
+                load_count = 200
+                processed = 0
 
-            if last_id:
-                self.stdout.write(f"Loading tweets since {last_id}...")
-            elif max_id:
-                self.stdout.write(f"Loading tweets before {max_id}...")
-            else:
-                self.stdout.write(f"Loading tweets...")
+                if last_id:
+                    self.stdout.write(f"Loading tweets since {last_id}...")
+                elif max_id:
+                    self.stdout.write(f"Loading tweets before {max_id}...")
+                else:
+                    self.stdout.write(f"Loading tweets...")
 
-            statuses = client.GetUserTimeline(
-                screen_name="BTS_SkyTrain",
-                since_id=last_id,
-                max_id=max_id,
-                count=load_count,
-                include_rts=False,
-                trim_user=True,
-                exclude_replies=True,
-            )
-            self.stdout.write(self.style.SUCCESS(f"Loaded {len(statuses)} tweets"))
+                statuses = client.GetUserTimeline(
+                    screen_name="BTS_SkyTrain",
+                    since_id=last_id,
+                    max_id=max_id,
+                    count=load_count,
+                    include_rts=False,
+                    trim_user=True,
+                    exclude_replies=True,
+                )
+                self.stdout.write(self.style.SUCCESS(f"Loaded {len(statuses)} tweets"))
 
-            for item in statuses:
-                if item.id in found_id:
-                    continue
+                for item in statuses:
+                    if item.id in found_id:
+                        continue
 
-                created = datetime.datetime.fromtimestamp(item.created_at_in_seconds, tz=datetime.timezone.utc)
-                tweets.append(Tweet(id=item.id, created_time=created, message=item.full_text, user="BTS_SkyTrain"))
+                    created = datetime.datetime.fromtimestamp(
+                        item.created_at_in_seconds, tz=datetime.timezone.utc
+                    )
+                    with transaction.atomic():
+                        # Use savepoint to indicate that we don't care
+                        # about errors
+                        try:
+                            Tweet(
+                                id=item.id,
+                                created_time=created,
+                                message=item.full_text,
+                                user="BTS_SkyTrain",
+                            ).save()
+                        except IntegrityError:
+                            pass
 
-                max_id = item.id
-                last_id = None
-                found_id.add(item.id)
-                processed += 1
+                    max_id = item.id
+                    last_id = None
+                    found_id.add(item.id)
+                    processed += 1
 
-            if processed == 0:
-                break
-
-        self.stdout.write(f"Pushing {len(tweets)} items to database")
-        Tweet.objects.bulk_create(tweets)
-        self.stdout.write(self.style.SUCCESS(f"Finished"))
+                if processed == 0:
+                    break
 
         cache.set("tweetdb:last_update", time.time())
