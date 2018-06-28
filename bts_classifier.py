fec1ad8baee2325c103a45b1d681e155fd6af90d
Sirakorn Lamyai <tanersirakorn@gmail.com>
2018-06-28 11:12:41 +0700

    :sparkle: Better model (98.254%)

--- bts_classifier.py
+++ bts_classifier.py
@@ -1,21 +1,16 @@
-from pathlib import Path
-
 from sklearn.naive_bayes import GaussianNB
 import pickle
 
-MODEL_FILE = Path(__file__).parent / 'model.pkl'
-
 def word_count(text):
-    keywords = ["ปกติ", "ตามปกติ", "ได้รับการแก้ไข", "นำออกจากระบบให้บริการ", "เคลื่อนที่ได้แล้ว", "สรุป", "ได้ตามปกติ", "กลับสู่สภาวะปกติ",
-        "ใช้งานได้แล้ว", "มีความล่าช้า", "ส่งผลกระทบ", "ความเร็วต่ำ",
-        "เกิดเหตุ", "ขัดข้อง", "จะล่าช้า", "แก้ไข", "เผื่อเวลา", "ช่วยเหลือ", "@"]
+    keywords = ["ปกติ", "ตามปกติ", "ได้รับการแก้ไข", "นำออกจากระบบให้บริการ", "เคลื่อนที่ได้แล้ว",
+                "สรุป", "ได้ตามปกติ", "กลับสู่สภาวะปกติ", "ทยอยเคลื่อนที่", "เรียบร้อย", "ใช้งานได้",
+                "มีความล่าช้า", "ส่งผลกระทบ", "ความเร็วต่ำ",
+                "เกิดเหตุ", "ขัดข้อง", "จะล่าช้า", "แก้ไข", "เผื่อเวลา", "ช่วยเหลือ",
+                "ชี้แจง", "@", "t.co", "ระบบควบคุม"]
     return [text.count(keyword) for keyword in keywords]
 
 def classify(text):
     labels = [None, 'normal', 'delay', 'disrupted']
     x = [word_count(text)]
-
-    with MODEL_FILE.open('rb') as fp:
-        clf = pickle.load(fp)
-
+    clf = pickle.load(open('model.pkl', 'rb'))
     return labels[clf.predict(x)[0]]
